5683fb6b44985494027a5b55b8713bac90e8d4a6
iradtaufique <iradukunda.ta@gmail.com>
2021-05-25 12:47:29 +0200

    make change in setting

--- showcaseproject/settings.py
+++ showcaseproject/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure--od_boviyo25j&wwpnxk+t@=)hl5^w3p3e3q1+8-qi_w)pix4d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['maliki.pythonanywhere.com']
 
 
 # Application definition
